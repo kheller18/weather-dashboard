573dad5f9eecc436312d302a81ca222753c3b06f
Keenan Heller <keenanheller@MacBook-Pro.local>
2020-07-19 23:22:00 -0700

    added javascript function to display cities uppercase. partially formatted date, cleaned up today's weather display

--- script.js
+++ script.js
@@ -2,53 +2,31 @@
 $(document).ready(function() {
     var city;
     var temp;
-    var a;
-    var b;
     var weatherTemp;
-    var post;
-    icon = [];
+    var icons = [];
+    var apiKey = "54ea5276d8943e943c85e5932fdd782a";
+    var cities = [];
+    var today, icon, uv, forecast, temp, lat, long, weatherTemp, lastCity, tempDate;
+    var forecastData = [];
 
-    
-    $("form").submit(function(e) {
-        e.preventDefault();
-        city = $("#searchCity").val();
+    displayLastCity();
+    renderCities();
+/* model array 
 
-        console.log(city);
-        if (city != null || city != undefined || city != "") {   
-            
-            // fetchCity(city).then(function(data) {
-            //     weatherTemp = data.weather[0].icon;
-            //     icon.push(weatherTemp);
-            //     weatherTemp.toString();
-            //     console.log(data);
-            //     $(".city").html(`<h1>${ data.name }</h1>`);
-            //     $(".humidity").html(`<p>Humidity: ${ data.main.humidity }</p>`);
-            //     $(".windSpeed").html(`<p>Wind Speed: ${ data.wind.speed }</p>`);
-            //     post = data;
-            //     console.log(weatherTemp);
-            //     return weatherTemp;
-            // }).then(fetchImg(weatherTemp).then(function(data) {
-            //     console.log(data)
-            
-            // })
-            // );
-            // console.log(weatherTemp);
-            // fetchImg(weatherTemp).then(function(data){
-            //     console.log(data);
-            
-            
-            // });       
-            var today, icon, uv, forecast;
-            var img = new Image();
-            var lat, long;
 
-            fetch("http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=54ea5276d8943e943c85e5932fdd782a")
+
+
+
+*/
+    function generateWeatherData(city) {
+        if (city != null || city != undefined || city != "") {               
+            fetch(`http://api.openweathermap.org/data/2.5/weather?q=${ city }&appid=${ apiKey }`)
                 .then(function (response) {
                     return response.json();
                 }).then(function (data) {
                     weatherTemp = data.weather[0].icon
                     today = data;
-                    return fetch("http://openweathermap.org/img/wn/" + weatherTemp + "@2x.png");
+                    return fetch(`http://openweathermap.org/img/wn/${ weatherTemp }@2x.png`);
                 }).then(function (response) {
                     return response;
                 }).then(function (data) {
@@ -56,12 +34,12 @@ $(document).ready(function() {
                     console.log(icon);
                     lat =  today.coord.lat;
                     long = today.coord.lon;
-                    return fetch("http://api.openweathermap.org/data/2.5/uvi?appid=54ea5276d8943e943c85e5932fdd782a&lat=" + lat + "&lon=" + long)
+                    return fetch(`http://api.openweathermap.org/data/2.5/uvi?appid=${ apiKey }&lat=${ lat }&lon=${ long }`)
                 }).then(function (response) {
                     return response.json()
                 }).then(function (data) {
                     uv = data;
-                    return fetch("http://api.openweathermap.org/data/2.5/forecast?q=Seattle&appid=54ea5276d8943e943c85e5932fdd782a")
+                    return fetch(`http://api.openweathermap.org/data/2.5/forecast?q=${ city }&appid=${ apiKey }`)
                 }).then(function (response) {
                     return response.json()
                 }).then (function (data) {
@@ -70,63 +48,134 @@ $(document).ready(function() {
                     console.log(icon);
                     console.log(uv);
                     console.log(forecast);
-                    var img = new Image();
-                    img.src = "http://openweathermap.org/img/wn/01d@2x.png";
-                    $(".city").html(`<h1>${ today.name } <img src=${ img.src } /></h1>`);
+                    temp = ((today.main.temp -273.15) * 1.8 + 32).toFixed(1);
+                    tempDate = uv.date_iso;
+                    console.log(tempDate);
+                    tempDate = tempDate.split("T");
+                    tempDate = tempDate[0];
+                    //tempDate.replace('-','/');
+                    console.log(tempDate);
+
+                    getForecast(today, icon, uv, forecast);
+
+
+
+                    
+                    renderCities();
+
                 }).catch(function (error) {
                     console.log(error);
                 });  
-                            
-                            
-                           
-                            //$(".city").append('<img src=getpic />');
-                            //$(".temperature").text((temp));
-                            //$(".humidity").text((data.main.humidity));
-                            //$(".windSpeed").text((data.wind.speed));
-                           // $(".uvIndex").text((data.name));
-                            //console.log(city);
         }
+    }
+    
+    
+    function renderCities() {
+        $(".search-History").empty();
 
-    });
+        if (JSON.parse(localStorage.getItem("cities")) != null) {
+            cities = JSON.parse(localStorage.getItem("cities"));
+        }
+        for (var i = cities.length - 1; i >= 0; i--) {
+            var a = $("<li>");
+            a.text(cities[i])
+            $(".search-History").append(a);
+        }
+    }
 
+    function storeCities(city) {
+        city = cityUpperCase(city);
+        if (JSON.parse(localStorage.getItem("cities")) != null) {
+            cities = JSON.parse(localStorage.getItem("cities"));
+        }
+        cities.push(city);
+        localStorage.setItem('cities', JSON.stringify(cities));
+        renderCities();
+    }
+
+    function displayLastCity() {
+        if (JSON.parse(localStorage.getItem("cities")) != null) {
+            cities = JSON.parse(localStorage.getItem("cities"));
+            lastCity = cities[cities.length - 1];
+            generateWeatherData(lastCity);
+        }
         
 
-        async function fetchCity(city) {
-            const response = await fetch("http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=54ea5276d8943e943c85e5932fdd782a")
-            const data = await response.json()
-            return data;
-        }
+    }
+
+    //function formatDate() {
+
+    //}
+
+    
+
+    function cityUpperCase(city) {
+        return city.replace(/\w\S*/g, function(txt){
+            return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+        });
+    }
+
+    function getForecast (today, icon, uv, forecast) {
+
+        console.log(today);
+        console.log(icon);
+        console.log(uv);
+        console.log(forecast);
+
          
-        async function fetchImg(wpic) {
-            const response = await fetch("http://openweathermap.org/img/wn/" + wpic + "@2x.png")
-            const data = await response
-            return data;
+
+        $(".city").html(`<h2>${ today.name } (${ tempDate }) <img src=${ icon.url } /></h2>`);
+        $(".temperature").html(`<p>Temperature: ${ temp } F</p>`);
+        $(".humidity").html(`<p>Humidity: ${ today.main.humidity }%</p>`);
+        $(".windSpeed").html(`<p>Wind Speed: ${ today.wind.speed } MPH</p>`);
+        $(".uvIndex").html(`<p>UV Index: ${ uv.value }</p>`);
+
+        for (i = 0; i < forecast.list.length - 1; i++) {
+            console.log("hello")
         }
+
+        $("#forecast-1").html((`<h6> ${ forecast.list[0].dt_txt }  </h6>`));
+        $("#forecast-1").append(`<p> ${ forecast.list[0].main.temp} F</p>`);
+        $("#forecast-1").append(`<p>Temp: ${ forecast.list[0].main.temp} F</p>`);
+        $("#forecast-1").append(`<p>Humidity: ${ forecast.list[0].main.humidity}%</p>`);
+        $("#forecast-2").html((`<h6> ${ forecast.list[8].dt_txt }  </h6>`));
+        $("#forecast-2").append(`<p>Temp: ${ forecast.list[8].main.temp} F</p>`);
+        $("#forecast-2").append(`<p>Humidity: ${ forecast.list[8].main.humidity}%</p>`);
+        $("#forecast-3").html((`<h6> ${ forecast.list[16].dt_txt }  </h6>`));
+        $("#forecast-3").append(`<p>Temp: ${ forecast.list[16].main.temp} F</p>`);
+        $("#forecast-3").append(`<p>Humidity: ${ forecast.list[16].main.humidity}%</p>`);
+        $("#forecast-4").html((`<h6> ${ forecast.list[24].dt_txt }  </h6>`));
+        $("#forecast-4").append(`<p>Temp: ${ forecast.list[24].main.temp} F</p>`);
+        $("#forecast-4").append(`<p>Humidity: ${ forecast.list[24].main.humidity}%</p>`);
+        $("#forecast-5").html((`<h6> ${ forecast.list[32].dt_txt }  </h6>`));
+        $("#forecast-5").append(`<p>Temp: ${ forecast.list[32].main.temp} F</p>`);
+        $("#forecast-5").append(`<p>Humidity: ${ forecast.list[32].main.humidity}%</p>`);
+    }
+
+    //function getIcons() {
+
+    //}
+
+    $("form").submit(function(e) {
+        e.preventDefault();
+        city = $("#searchCity").val();
+        storeCities(city);
+        generateWeatherData(city);
+    });
+
+    async function fetchCity(city) {
+        const response = await fetch("http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=54ea5276d8943e943c85e5932fdd782a")
+        const data = await response.json()
+        return data;
+    }
+        
+    async function fetchImg(wpic) {
+        const response = await fetch("http://openweathermap.org/img/wn/" + wpic + "@2x.png")
+        const data = await response
+        return data;
+    }
                     
        
-
+    
 
 });
-
-   
-        // console.log("hello");
-        // fetch("http://api.openweathermap.org/data/2.5/weather?q=Seattle&appid=54ea5276d8943e943c85e5932fdd782a")
-        //     .then(
-        //         function(response) {
-        //             console.log(response);
-                
-        //             response.json().then(function(data) {
-        //                 console.log(data);
-                    
-                    
-        //                 console.log(city)
-                    
-                    
-                    
-        //             });
-                    
-        //         }
-        //     )
-            
-    
-    
